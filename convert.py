e5ac4e2de45936eaf19d8620339405fb917f0135
jvittor <102699222+jvittor@users.noreply.github.com>
2024-07-22 09:02:47 -0300

    :tada: first commit

---
+++ convert.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+import time
+
+# Função para geocodificação
+def geocode_address(address, geolocator, index):
+    try:
+        location = geolocator.geocode(address)
+        print(f"{index}: Geocodificado '{address}'")
+        return location.latitude, location.longitude
+    except Exception as e:
+        print(f"{index}: Falha ao geocodificar '{address}' - {e}")
+        return None, None
+
+file_path = 'address.xlsx' 
+df = pd.read_excel(file_path)
+
+geolocator = Nominatim(user_agent="geoapiExercises")
+
+# taxa de limite
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+
+latitudes = []
+longitudes = []
+
+start_time = time.time()
+
+for i, row in df.iterrows():
+    lat, lon = geocode_address(f"{row['endereco']}, {row['municipio']}", geolocator, i)
+    latitudes.append(lat)
+    longitudes.append(lon)
+    if i % 10 == 0: 
+        print(f"processados {i+1}/{len(df)} endereços")
+
+df['latitude'] = latitudes
+df['longitude'] = longitudes
+
+end_time = time.time()
+print(f"tempo total de execução: {end_time - start_time:.2f} segundos")
+
+result_df = df[['latitude', 'longitude']]
+
+
+output_file_path = 'novo_arquivo.xlsx' 
+result_df.to_excel(output_file_path, index=False)
+
+print("tabela de latitude e longitude criada com sucesso!")
